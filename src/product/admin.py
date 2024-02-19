bf2b3f23f78b36f9a1ae583ac695a101dbcee5f1
Tarik SADKHI <tarik.sadkhi@gmail.com>
2024-02-19 11:54:21 +0100

    adding category filtering + flattening get product display

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 
-from .models import Category, Brand, Product
+from .models import Category, Brand, Product, ProductLine
+
+
+class ProductLineInline(admin.TabularInline):
+    model = ProductLine
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductLineInline]
 
 
 admin.site.register(Category)
 admin.site.register(Brand)
-admin.site.register(Product)
+admin.site.register(ProductLine)
